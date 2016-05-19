33955ad3d87aa4b4346e8d9abb0c1084669e16e2
Christian Amaral <darthcas@gmail.com>
2016-05-19 14:15:56 -0300

    documentation and type hinting

---
+++ README.md
@@ -0,0 +1,6 @@
+# Configuration
+
+Expects the following environment variables to be defined: 
+- **LOGSTASH_HTTP_URL**
+- **LOGSTASH_HTTP_USER**
+- **LOGSTASH_HTTP_PWD**
--- Tetris/MonoStash/MonoStash.php
+++ Tetris/MonoStash/MonoStash.php
@@ -7,17 +7,17 @@ use Monolog\Handler\StreamHandler;
 
 class MonoStash extends Logger
 {
-    public function __construct($app)
+    public function __construct(string $appName)
     {
         $monostash = new Handler();
 
-        $monostash->setFormatter(new Formatter("{$app}@" . gethostname()));
+        $monostash->setFormatter(new Formatter("{$appName}@" . gethostname()));
         $handlers[] = $monostash;
 
         if (PHP_SAPI === 'cli') {
             $handlers[] = new StreamHandler("php://stdout");
         }
 
-        parent::__construct($app, $handlers);
+        parent::__construct($appName, $handlers);
     }
 }
