4a0ac4918bf6c689e2dfe5155710477864a7e140
caostar <beterrabas@gmail.com>
2019-11-06 14:00:19 -0300

    removing logstash

--- src/Tetris/MonoStash/Handler.php
+++ src/Tetris/MonoStash/Handler.php
@@ -11,10 +11,10 @@ class Handler extends AbstractProcessingHandler
     public function write(array $record)
     {
         try {
-            HttpRequest::post(getenv('LOGSTASH_HTTP_URL'), $record['formatted'], 'application/json')
+            /*HttpRequest::post(getenv('LOGSTASH_HTTP_URL'), $record['formatted'], 'application/json')
                 ->mime('text/plain')
                 ->authenticateWith(getenv('LOGSTASH_HTTP_USER'), getenv('LOGSTASH_HTTP_PWD'))
-                ->send();
+                ->send();*/
         } catch (Exception $e) {
 //            echo "{$e->getCode()} : {$e->getMessage()}\n";
 //            echo $e->getTraceAsString();
